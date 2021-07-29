31b4a02dc541558f71287980b5e4e0418143c5d4
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-29 19:54:43 +0300

    Added config file spring.xml for creating beans of classes marked with @Component

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car implements Vehicle {
+
+    public void drive() {
+        System.out.println("The car is driving");
+    }
+}
