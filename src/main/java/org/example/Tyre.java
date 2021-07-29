757bc9281471070e5db460f1cbfce2074245658a
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-29 20:25:23 +0300

    Made constructor injection in spring.xml

--- src/main/java/org/example/Tyre.java
+++ src/main/java/org/example/Tyre.java
@@ -4,6 +4,10 @@ public class Tyre {
 
     private String brand;
 
+    public Tyre(String brand) {
+        this.brand = brand;
+    }
+
     public String getBrand() {
         return brand;
     }
