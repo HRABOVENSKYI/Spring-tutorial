35a36ac40dfc37d701efbb7f12a09b501b6cded3
HRABOVENSKYI <teodor.gr.2002@gmail.com>
2021-07-29 20:35:54 +0300

    @Autowired for injecting Tyre obj in Car

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,10 +8,9 @@ public class App {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-//        Vehicle obj = (Vehicle) context.getBean("bike");
-//        obj.drive();
+        Car obj = (Car) context.getBean("car");
+        Car car;
+        obj.drive();
 
-        Tyre t = (Tyre) context.getBean("tyre");
-        System.out.println(t);
     }
 }
--- src/main/java/org/example/Tyre.java
+++ src/main/java/org/example/Tyre.java
@@ -1,12 +1,11 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Tyre {
 
     private String brand;
-
-    public Tyre(String brand) {
-        this.brand = brand;
-    }
 
     public String getBrand() {
         return brand;
